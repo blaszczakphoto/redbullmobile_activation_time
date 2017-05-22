8e7fa93fba556f0947841b167eab0f9dbc32b160
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-05-22 13:44:55 +0200

    add logic

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,4 +1,13 @@
 class CalculatorsController < ApplicationController
   def index
   end
+
+  def calculate
+    next_activation = DateTime.parse(params['datetime']).to_time +  719*60*60
+    redirect_to result_path(next_activation: next_activation)
+  end
+
+  def result
+    @next_activation = params['next_activation']
+  end
 end
